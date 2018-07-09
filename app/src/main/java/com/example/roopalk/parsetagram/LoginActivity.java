f1e6e9d736dfa8dcdea62206bca2d1c7aa0153dd
Roopal Kondepudi <roopalk@fb.com>
2018-07-09 15:06:21 -0700

    PLEASE WORK

---
+++ app/src/main/java/com/example/roopalk/parsetagram/LoginActivity.java
@@ -0,0 +1,74 @@
+package com.example.roopalk.parsetagram;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity
+{
+    //Using ButterKnife to bind the views to the IDs
+    @BindView(R.id.etUsername) EditText etUsername;
+    @BindView(R.id.etPassword) EditText etPassword;
+    @BindView(R.id.btnLogin)Button btnLogin;
+
+    private final static String TAG = "LoginActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        btnLogin.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                //getting the username and password from the edit text
+                final String username = etUsername.getText().toString();
+                final String password = etPassword.getText().toString();
+
+                //calling the login method, where we can use Parse
+                try
+                {
+                    login(username, password);
+                }
+                catch (ParseException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void login(String username, String password) throws ParseException {
+        //setting up parse configuration
+        ParseUser.logInInBackground(username, password, new LogInCallback()
+        {
+            //checks if the log in has actually been completed successfully
+            @Override
+            public void done(ParseUser user, ParseException e)
+            {
+                if(e != null)
+                {
+                    Log.d(TAG, "Login successful!");
+                }
+                else
+                {
+                    Log.e(TAG, " Login failure.");
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
