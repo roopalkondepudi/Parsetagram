577117e8ec1d81cdfa5d81f3193aabcf67abe70e
Roopal Kondepudi <roopalk@fb.com>
2018-07-11 10:25:27 -0700

    added camera

--- app/src/main/java/com/example/roopalk/parsetagram/HomeActivity.java
+++ app/src/main/java/com/example/roopalk/parsetagram/HomeActivity.java
@@ -1,9 +1,15 @@
 package com.example.roopalk.parsetagram;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -12,6 +18,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.roopalk.parsetagram.model.Post;
 import com.parse.FindCallback;
@@ -21,6 +29,7 @@ import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 import butterknife.BindView;
@@ -29,25 +38,17 @@ import butterknife.ButterKnife;
 public class HomeActivity extends AppCompatActivity
 {
 
-    //request image capture
+    public final String APP_TAG = "Camera";
     static final int REQUEST_IMAGE_CAPTURE = 3;
-
-    //intent to take a picture
-    private void takePicture()
-    {
-        Intent photoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if(photoIntent.resolveActivity(getPackageManager()) != null)
-        {
-            startActivityForResult(photoIntent, REQUEST_IMAGE_CAPTURE);
-        }
-
-    }
+    public String photoFileName = "photo.jpg";
+    File photoFile;
 
     public static final String TAG = "HomeActivity";
-    private static final String imagePath = "/DCIM/Camera/IMG_20180710_114531.jpg";
+    //private static final String imagePath = "/DCIM/Camera/IMG_20180710_114531.jpg";
     @BindView(R.id.etDescription) EditText etDescription;
     @BindView(R.id.create_btn) Button btnCreate;
     @BindView(R.id.refresh_btn) Button btnRefresh;
+    @BindView(R.id.camera_btn) ImageView ivCamera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,11 +69,8 @@ public class HomeActivity extends AppCompatActivity
                 //Toast.makeText(getBaseContext(), "whatever", Toast.LENGTH_LONG).show();
                 final String description = etDescription.getText().toString();
                 final ParseUser user = ParseUser.getCurrentUser();
+                final ParseFile parseFile = new ParseFile(photoFile);
 
-                String path = Environment.getExternalStorageDirectory().getPath();
-
-                final File file = new File(path+imagePath);
-                final ParseFile parseFile = new ParseFile(file);
                 parseFile.saveInBackground(new SaveCallback() {
                     @Override
                     public void done(ParseException e) {
@@ -82,6 +80,13 @@ public class HomeActivity extends AppCompatActivity
                             e.printStackTrace();
                     }
                 });
+            }
+        });
+
+        ivCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onLaunchCamera(v);
             }
         });
     }
@@ -153,5 +158,88 @@ public class HomeActivity extends AppCompatActivity
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    //when Camera is launched
+    public void onLaunchCamera(View view) {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference to access to future access
+        photoFile = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), "share_image_" + System.currentTimeMillis() + ".png");
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(HomeActivity.this, "com.codepath.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    // Returns the File for a photo stored on disk given the fileName
+    public File getPhotoFileUri(String fileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), APP_TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(APP_TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+
+        return file;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE) {
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                // RESIZE BITMAP, see section below
+                // Load the taken image into a preview
+                ImageView ivPreview = (ImageView) findViewById(R.id.ivPreview);
+                ivPreview.setImageBitmap(takenImage);
+            } else { // Result was a failure
+                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    public Bitmap rotateBitmapOrientation(String photoFilePath) {
+        // Create and configure BitmapFactory
+        BitmapFactory.Options bounds = new BitmapFactory.Options();
+        bounds.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(photoFilePath, bounds);
+        BitmapFactory.Options opts = new BitmapFactory.Options();
+        Bitmap bm = BitmapFactory.decodeFile(photoFilePath, opts);
+        // Read EXIF Data
+        ExifInterface exif = null;
+        try {
+            exif = new ExifInterface(photoFilePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String orientString = exif.getAttribute(ExifInterface.TAG_ORIENTATION);
+        int orientation = orientString != null ? Integer.parseInt(orientString) : ExifInterface.ORIENTATION_NORMAL;
+        int rotationAngle = 0;
+        if (orientation == ExifInterface.ORIENTATION_ROTATE_90) rotationAngle = 90;
+        if (orientation == ExifInterface.ORIENTATION_ROTATE_180) rotationAngle = 180;
+        if (orientation == ExifInterface.ORIENTATION_ROTATE_270) rotationAngle = 270;
+        // Rotate Bitmap
+        Matrix matrix = new Matrix();
+        matrix.setRotate(rotationAngle, (float) bm.getWidth() / 2, (float) bm.getHeight() / 2);
+        Bitmap rotatedBitmap = Bitmap.createBitmap(bm, 0, 0, bounds.outWidth, bounds.outHeight, matrix, true);
+        // Return result
+        return rotatedBitmap;
     }
 }
