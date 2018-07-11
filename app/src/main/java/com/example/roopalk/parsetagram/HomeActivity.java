0b0f25f0582cec3c35b49cbfd2c970275b69cbc2
Roopal Kondepudi <roopalk@fb.com>
2018-07-11 15:59:04 -0700

    moved camera stuff to another activity, but added icon to the toolbar

--- app/src/main/java/com/example/roopalk/parsetagram/HomeActivity.java
+++ app/src/main/java/com/example/roopalk/parsetagram/HomeActivity.java
@@ -1,36 +1,23 @@
 package com.example.roopalk.parsetagram;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
-import android.media.ExifInterface;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.support.v4.content.FileProvider;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.Toast;
 
+import com.example.roopalk.parsetagram.Adapter.PostAdapter;
 import com.example.roopalk.parsetagram.model.Post;
 import com.parse.FindCallback;
 import com.parse.ParseException;
-import com.parse.ParseFile;
-import com.parse.ParseUser;
-import com.parse.SaveCallback;
 
-import java.io.File;
-import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
@@ -39,65 +26,55 @@ import butterknife.ButterKnife;
 public class HomeActivity extends AppCompatActivity
 {
 
-    public final String APP_TAG = "Camera";
-    static final int REQUEST_IMAGE_CAPTURE = 3;
-    public String photoFileName = "photo.jpg";
-    File photoFile;
-
     public static final String TAG = "HomeActivity";
     //private static final String imagePath = "/DCIM/Camera/IMG_20180710_114531.jpg";
-    @BindView(R.id.etDescription) EditText etDescription;
-    @BindView(R.id.create_btn) Button btnCreate;
-    @BindView(R.id.refresh_btn) Button btnRefresh;
-    @BindView(R.id.camera_btn) ImageView ivCamera;
     @BindView(R.id.toolbar) Toolbar toolbar;
+    @BindView(R.id.rvPost) RecyclerView rvPosts;
+    ArrayList<Post> posts;
+    PostAdapter postAdapter;
+
+    private SwipeRefreshLayout swipeContainer;
+
+    private final int REQUEST = 32;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         ButterKnife.bind(this);
 
-        btnRefresh.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                loadTopPosts();
-            }
-        });
+        posts = new ArrayList<>();
 
-        btnCreate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Toast.makeText(getBaseContext(), "whatever", Toast.LENGTH_LONG).show();
-                final String description = etDescription.getText().toString();
-                final ParseUser user = ParseUser.getCurrentUser();
+        loadTopPosts();
 
-                File file = getPhotoFileUri(photoFileName);
-                final ParseFile parseFile = new ParseFile(file);
+        postAdapter = new PostAdapter(posts);
 
-                parseFile.saveInBackground(new SaveCallback() {
-                    @Override
-                    public void done(ParseException e) {
-                        if(e == null)
-                            createPost(description, parseFile, user);
-                        else
-                            e.printStackTrace();
-                    }
-                });
-            }
-        });
+        rvPosts.setLayoutManager(new LinearLayoutManager(this));
 
-        ivCamera.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onLaunchCamera(v);
-            }
-        });
+        rvPosts.setAdapter(postAdapter);
 
         setSupportActionBar(toolbar);
+
+        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
+        // Setup refresh listener which triggers new data loading
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // Your code to refresh the list here.
+                // Make sure you call swipeContainer.setRefreshing(false)
+                // once the network request has completed successfully.
+                loadTopPosts();
+                swipeContainer.setRefreshing(false);
+            }
+        });
+
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright, android.R.color.holo_green_light, android.R.color.holo_orange_light, android.R.color.holo_red_light);
+
     }
 
-    private void loadTopPosts()
+    public void loadTopPosts()
     {
         final Post.Query postsQuery = new Post.Query();
         postsQuery
@@ -110,6 +87,7 @@ public class HomeActivity extends AppCompatActivity
                 {
                     for(int i = 0; i < objects.size(); i++)
                     {
+                        posts.add(objects.get(i));
                         Log.d(TAG, "Post[" + i + "] = " + objects.get(i).getKeyDescription() + "\n username = " + objects.get(i).getKeyUser().getUsername());
                     }
                 }
@@ -121,132 +99,42 @@ public class HomeActivity extends AppCompatActivity
         });
     }
 
-    private void createPost(String description, ParseFile imageFile, ParseUser user)
-    {
-        final Post newPost = new Post();
-        newPost.setKeyDescription(description);
-        newPost.setKeyUser(user);
-        newPost.setKeyImage(imageFile);
-
-        newPost.saveInBackground(new SaveCallback() {
-            @Override
-            public void done(ParseException e) {
-                if(e == null)
-                {
-                    Log.d(TAG, "Create post success!");
-                }
-                else
-                {
-                    Log.e(TAG, "Create post failure");
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
-
     //make the menu
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
+        MenuInflater inflater = getMenuInflater(); //inflates the menu onto the toolbar
         inflater.inflate(R.menu.main_menu, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item) { //depending on the item selected, go to different activities
         switch(item.getItemId())
         {
-            case R.id.logout:
+            case(R.id.logout):
                 Intent intent = new Intent(HomeActivity.this, LogoutActivity.class);
                 startActivity(intent);
+                return true;
+            case(R.id.camera):
+                Intent cameraIntent = new Intent(HomeActivity.this, CameraActivity.class);
+                startActivityForResult(cameraIntent, REQUEST);
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
 
-    //when Camera is launched
-    public void onLaunchCamera(View view) {
-        // create Intent to take a picture and return control to the calling application
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Create a File reference to access to future access
-        photoFile = getPhotoFileUri(photoFileName);
-
-        // wrap File object into a content provider
-        // required for API >= 24
-        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
-        Uri fileProvider = FileProvider.getUriForFile(HomeActivity.this, "com.codepath.fileprovider", photoFile);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
-
-        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
-        // So as long as the result is not null, it's safe to use the intent.
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            // Start the image capture intent to take photo
-            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
-        }
-    }
-
-    // Returns the File for a photo stored on disk given the fileName
-    public File getPhotoFileUri(String fileName) {
-        // Get safe storage directory for photos
-        // Use `getExternalFilesDir` on Context to access package-specific directories.
-        // This way, we don't need to request external read/write runtime permissions.
-        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), APP_TAG);
-
-        // Create the storage directory if it does not exist
-        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
-            Log.d(APP_TAG, "failed to create directory");
-        }
-
-        // Return the file target for the photo based on filename
-        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
-
-        return file;
-    }
-
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE) {
-            if (resultCode == RESULT_OK) {
-                // by this point we have the camera photo on disk
-                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
-                // RESIZE BITMAP, see section below
-                // Load the taken image into a preview
-                ImageView ivPreview = (ImageView) findViewById(R.id.ivPreview);
-                ivPreview.setImageBitmap(takenImage);
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-            } else { // Result was a failure
-                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+        if(requestCode == REQUEST)
+        {
+            if(resultCode == RESULT_OK)
+            {
+                finish();
             }
         }
-    }
-
-    public Bitmap rotateBitmapOrientation(String photoFilePath) {
-        // Create and configure BitmapFactory
-        BitmapFactory.Options bounds = new BitmapFactory.Options();
-        bounds.inJustDecodeBounds = true;
-        BitmapFactory.decodeFile(photoFilePath, bounds);
-        BitmapFactory.Options opts = new BitmapFactory.Options();
-        Bitmap bm = BitmapFactory.decodeFile(photoFilePath, opts);
-        // Read EXIF Data
-        ExifInterface exif = null;
-        try {
-            exif = new ExifInterface(photoFilePath);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        String orientString = exif.getAttribute(ExifInterface.TAG_ORIENTATION);
-        int orientation = orientString != null ? Integer.parseInt(orientString) : ExifInterface.ORIENTATION_NORMAL;
-        int rotationAngle = 0;
-        if (orientation == ExifInterface.ORIENTATION_ROTATE_90) rotationAngle = 90;
-        if (orientation == ExifInterface.ORIENTATION_ROTATE_180) rotationAngle = 180;
-        if (orientation == ExifInterface.ORIENTATION_ROTATE_270) rotationAngle = 270;
-        // Rotate Bitmap
-        Matrix matrix = new Matrix();
-        matrix.setRotate(rotationAngle, (float) bm.getWidth() / 2, (float) bm.getHeight() / 2);
-        Bitmap rotatedBitmap = Bitmap.createBitmap(bm, 0, 0, bounds.outWidth, bounds.outHeight, matrix, true);
-        // Return result
-        return rotatedBitmap;
     }
 }
