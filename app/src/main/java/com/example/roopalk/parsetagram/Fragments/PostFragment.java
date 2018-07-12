57b69737c998a1968e13ab91a59c612ad85b462d
Roopal Kondepudi <roopalk@fb.com>
2018-07-12 16:21:47 -0700

    working on post fragment now

--- app/src/main/java/com/example/roopalk/parsetagram/Fragments/PostFragment.java
+++ app/src/main/java/com/example/roopalk/parsetagram/Fragments/PostFragment.java
@@ -1,110 +1,103 @@
 package com.example.roopalk.parsetagram.Fragments;
 
-import android.content.Context;
-import android.net.Uri;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.roopalk.parsetagram.R;
+import com.example.roopalk.parsetagram.model.Post;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
+import java.io.File;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link PostFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link PostFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class PostFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
-    public PostFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PostFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PostFragment newInstance(String param1, String param2) {
-        PostFragment fragment = new PostFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    private static final String TAG = "PostFragment";
+    //Binding views with ButterKnife
+    @BindView(R.id.ivPreview) ImageView ivPreview;
+    @BindView(R.id.etDescription) TextView etDescription;
+    @BindView(R.id.create_btn) Button btnCreate;
+    @BindView(R.id.refresh_btn) Button btnRefresh;
+    // The onCreateView method is called when Fragment should create its View object hierarchy,
+    // either dynamically or via XML layout inflation.
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        // Defines the xml file for the fragment
+        return inflater.inflate(R.layout.fragment_camera, parent, false);
     }
 
+    // This event is triggered soon after onCreateView().
+    // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_post, container, false);
+    public void onViewCreated(View view, Bundle savedInstanceState)
+    {
+        // Setup any handles to view objects here
+        // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
+        ButterKnife.bind(this, view);
+
+        btnCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(getBaseContext(), "whatever", Toast.LENGTH_LONG).show();
+                final String description = etDescription.getText().toString();
+                final ParseUser user = ParseUser.getCurrentUser();
+
+                FragmentManager fm = getFragmentManager();
+                CameraFragment fragm = (CameraFragment) fm.findFragmentById(R.id.camera_fragment);
+
+                File file = fragm.getPhotoFileUri(fragm.photoFileName);
+
+                final ParseFile parseFile = new ParseFile(file);
+
+                parseFile.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if(e == null)
+                            createPost(description, parseFile, user);
+                        else
+                            e.printStackTrace();
+                    }
+                });
+            }
+        });
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
+    private void createPost(String description, ParseFile imageFile, ParseUser user)
+    {
+        final Post newPost = new Post(); //make a new post
+        newPost.setKeyDescription(description); //set all the attributes of the post
+        newPost.setKeyUser(user);
+        newPost.setKeyImage(imageFile);
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
+        newPost.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) { //save the post to Parse
+                if(e == null)
+                {
+//                    Intent returnIntent = new Intent();
+//                    setResult(REQUEST, returnIntent);
+//                    Log.d(TAG, "Create post success!");
+//                    finish();
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+                    //make a method call to host activity
+                }
+                else
+                {
+//                    Log.e(TAG, "Create post failure");
+//                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
